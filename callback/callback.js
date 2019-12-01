3e16e2f5182eda757dc56a2f20e08839066bf9b0
Steven Ren <rsw_mmo@hotmail.com>
2019-12-01 19:57:52 +0800

    learn on 12.1 at 7:57 pm

--- callback/callback.js
+++ callback/callback.js
@@ -1,10 +1,35 @@
-const doSomething = callback => {
-  setTimeout(() => {
-    callback(undefined, "normal message");
-  }, 2000);
-};
+///////////////// Callback function /////////////////////
+// const doSomething = callback => {
+//   setTimeout(() => {
+//     callback(undefined, "normal message");
+//   }, 2000);
+// };
 
-doSomething((err, res) => {
-  if (err) return console.log("ops:", err);
-  console.log("form res:", res);
+// doSomething((err, res) => {
+//   if (err) return console.log("ops:", err);
+//   console.log("form res:", res);
+// });
+
+///////////////// Promise function /////////////////////
+const doSomething = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    let aa = [1, 3, 5];
+    aa === undefined ? reject(new Error("siwen error")) : resolve(aa);
+  }, 2000);
 });
+
+doSomething
+  .then(resulte => console.log("Success!", resulte))
+  .catch(error => console.error(error));
+
+///////////////// Async function /////////////////////
+async function getData(params) {
+  try {
+    const resulte = await doSomething;
+    console.log("Success!", resulte);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+// getData();
